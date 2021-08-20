b4ca27069131968dc0b9e320a1d4347000bfc61c
elefteroff <elefteroff@mac.com>
2021-08-20 08:49:16 -0700

    Initial Commit

---
+++ Fizzbuzz.js
@@ -0,0 +1,16 @@
+var x = 0;
+for(var i = 1; i <= 100; i++) {
+        x  += 1;
+    if(x % 3 == 0 && x % 5 == 0) {
+        console.log("FizzBuzz");
+    }
+        else if(x % 3 == 0) {
+            console.log("Fizz");
+    }
+        else if(x % 5 == 0) {
+            console.log("Buzz");
+    }
+        else {
+            console.log(x);
+    }
+}
